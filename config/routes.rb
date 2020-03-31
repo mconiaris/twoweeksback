40277b811da2c4764570000fec5ef5025deea5d3
mconiaris <mconiaris@gmail.com>
2020-03-31 10:42:47 -0400

    Set routes for index and about pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
 
   root 'welcome#index'
+
+  get 'index', to: 'welcome#index'
+	get 'about', to: 'welcome#about'
+
+	get '*path', to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
