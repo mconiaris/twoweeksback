8a6bad60b3af7afb38565553abb5151d5a6bb1f2
mconiaris <mconiaris@gmail.com>
2020-04-09 18:02:47 -0400

    Added a stories resource to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 	get 'about', to: 'welcome#about'
 
 	get '*path', to: 'welcome#index'
+
+	resources :stories
+	
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
