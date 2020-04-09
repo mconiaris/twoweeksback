9f9241f87edf4c6b45e702bd93e8efe6020750a0
mconiaris <mconiaris@gmail.com>
2020-04-09 18:20:15 -0400

    Moved wildcard route to bottom of routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'index', to: 'welcome#index'
 	get 'about', to: 'welcome#about'
 
-	get '*path', to: 'welcome#index'
 
 	resources :stories
 	
+	get '*path', to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
