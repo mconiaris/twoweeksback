6be0e421b9880d3cc841e712da06006922a1ebc5
mconiaris <mconiaris@gmail.com>
2020-04-09 20:02:39 -0400

    Added note.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,10 @@
 class StoriesController < ApplicationController
 
+
+	# A frequent practice is to place the standard CRUD 
+	# actions in each controller in the following order: 
+	# index, show, new, edit, create, update and destroy. 
+
 	def new
 		
 	end
