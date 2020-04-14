74d93efac3e4dae35f57ddf1b2cd0658b543b0eb
mconiaris <mconiaris@gmail.com>
2020-04-14 16:24:53 -0400

    Added another validation to story model.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,4 @@
 class Story < ApplicationRecord
-	validates :title, presence: true,
-                    length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 5 }
+  validates :blurb, presence: true, length: { minimum: 10 }
 end
