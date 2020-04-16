3347d09485c775c414dc914c75d584bf8c0140d7
mconiaris <mconiaris@gmail.com>
2020-04-16 17:36:55 -0400

    Added validation for date in story model.

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,5 @@ class Story < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 5 }
   validates :blurb, presence: true, length: { minimum: 10 }
   validates :link, presence: true
+  validates :date, presence: true
 end
