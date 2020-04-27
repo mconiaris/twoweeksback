0c603639c0e8f88395f3bf905519ffee3196de4f
mconiaris <mconiaris@gmail.com>
2020-04-27 12:59:04 -0400

    Changed validation for titles in the Story model so that the minimum is 5 and the maximum is 25.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ApplicationRecord
-	validates :title, presence: true, length: { minimum: 5 }
+	validates :title, presence: true, length: { 5..25 }
   validates :blurb, presence: true, length: { minimum: 10 }
   validates :link, presence: true
   validates :date, presence: true
